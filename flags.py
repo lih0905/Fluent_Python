a4cc4cac94cf26546ea19029be91a722bd9c3db4
Inhwan Lee <lih0905@gmail.com>
2020-06-13 18:58:22 +0900

    Chap 17.1 *.py

---
+++ flags.py
@@ -0,0 +1,46 @@
+# flags.py
+
+import os
+import time
+import sys
+import requests
+
+# 국기 사이트가 뻗었으므로 걍 내 이미지로 하자...
+POP20_CC = ('8_1 8_2 8_3 8_4 8_5 '
+           '13_1 13_2 13_3 15_1').split()
+
+#BASE_URL = 'http://flupy.org/data/flags'
+BASE_URL = 'https://github.com/lih0905/Fluent_Python/raw/master/images/'
+
+DEST_DIR = 'downloads/'
+
+def save_flag(img, filename):
+    path = os.path.join(DEST_DIR, filename)
+    with open(path, 'wb') as fp:
+        fp.write(img)
+        
+def get_flag(cc):
+    url = '{}/{cc}.PNG'.format(BASE_URL, cc=cc.lower())
+    resp = requests.get(url)
+    return resp.content
+
+def show(text):
+    print(text, end=' ')
+    sys.stdout.flush()
+
+def download_many(cc_list):
+    for cc in sorted(cc_list):
+        image = get_flag(cc)
+        show(cc)
+        save_flag(image, cc.lower() + '.PNG')
+    return len(cc_list)
+
+def main(download_many):
+    t0 = time.time()
+    count = download_many(POP20_CC)
+    elapsed = time.time() - t0
+    msg = '\n{} flags downloaded in {:.2f}s'
+    print(msg.format(count, elapsed))
+    
+if __name__ == '__main__':
+    main(download_many)
---
+++ flags_threadpool.py
@@ -0,0 +1,31 @@
+# flags_threadpool.py
+
+from concurrent import futures
+
+# flags 모듈의 함수들 재사용
+from flags import save_flag, get_flag, show, main
+
+# ThreadPoolExecutor에서 사용할 최대 스레드 수
+MAX_WORKERS = 20
+
+# 하나의 이미지를 내려받을 함수
+def download_one(cc):
+    image = get_flag(cc)
+    show(cc)
+    save_flag(image, cc.lower() + '.PNG')
+    return cc
+
+def download_many(cc_list):
+    # 작업자 스레드 수
+    workers = min(MAX_WORKERS, len(cc_list))
+    # ThreadPoolExecutor 객체 생성
+    with futures.ThreadPoolExecutor(workers) as executor:
+        # map 메서드는 여러 스레드에 의해 download_one() 함수를 동시에 호출한다
+        # 내장된 map() 함수와 비슷젠하며, 각 함수가 반환한 값을 다담은 제너레이터 반환
+        res = executor.map(download_one, sorted(cc_list))
+    
+    # 결과 반환. 호출한 함수중 하나라도 예외를 발생시키면 여기서 에러 발생
+    return len(list(res))
+
+if __name__ == '__main__':
+    main(download_many)
